3699eb5a0be8591a3eb69f98523d5c1b925d0244
andreaskolshorn <andreaskolshorn@hotmail.com>
2021-02-23 00:04:18 -0800

    Implement matrix operations as a class. Update main() calls to use matrixOps class.

---
+++ MatrixRotate/MatrixOps.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatrixOps
+{
+    public class MatrixOps
+    {
+        public static int instances=0;
+ 
+        public MatrixOps()
+        {
+
+            instances =instances+1;   
+            Console.WriteLine($"Create Matrix Ops Object cnt={instances.ToString()} ");
+        }
+
+        public void Rotate_90(List<List<int>> matrix, int size)
+        {
+            int tail;
+
+            for (int box = 0; box < size / 2; box++)
+                for (int runner = box; runner < size - box - 1; runner++)
+                {
+                    tail = matrix[box][runner];
+                    matrix[box][runner] = matrix[size - runner - 1][box];        //TL
+                    matrix[size - runner - 1][box] = matrix[size - box - 1][size - runner - 1]; //BL
+                    matrix[size - box - 1][size - runner - 1] = matrix[runner][size - box - 1];        //BR
+                    matrix[runner][size - box - 1] = tail;                              //TR
+                }
+        }
+
+        public static void CreateDataMatrix(List<List<int>> dataMatrix, int size, int seed)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                var vi = new List<int>();
+
+                for (int j = 0; j < size; j++)
+                {
+                    vi.Insert(j, seed);
+                    seed++;
+                }
+
+                dataMatrix.Insert(i, vi);
+            }
+        }
+
+        public List<List<int>> MakeLeftOnes(int size)
+        {
+            /* Create a square matrix of ones in lower left side including diagonal.
+            * e.g.     1 0 0
+            *          1 1 0
+            *          1 1 1
+            */
+
+            int cnt;
+            var matrix = new List<List<int>>();
+
+            for (int i = 0; i < size; i++)
+            {
+                var vi = new List<int>();
+                cnt = 0;
+
+                for (int j = 0; j < size; j++)
+                   if (cnt < i + 1)
+                    {
+                        vi.Insert(j, 1);
+                        cnt++;
+                    }
+                    else
+                        vi.Insert(j, 0);
+
+                matrix.Insert(i, vi);
+            }
+
+            return matrix;
+        }
+
+        public List<List<int>> Multiply (List<List<int>> leftMatrix, List<List<int>> rightMatrix, int size)
+        {
+            var resultMatrix = new List<List<int>>(); 
+
+            for (int i = 0; i < size; i++)
+            {
+                var row = new List<int>();
+                for (int j = 0; j < size; j++)
+                {
+                    int tmp = 0;
+
+                    for (int r = 0; r < size; r++)
+                        tmp += leftMatrix[i][r] * rightMatrix[r][j];
+
+                    row.Insert(j, tmp);
+                }
+                resultMatrix.Insert(i, row);
+            }
+            return resultMatrix;
+        }
+
+        public void List(List<List<int>> matrix, int size, string msg)
+        {
+            Console.WriteLine();
+            Console.WriteLine(msg);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    Console.Write(matrix[i][j].ToString() + " ");
+                Console.WriteLine();
+            }
+        }
+
+
+
+    }   //End Matrix Class
+}   // End Namespace 
--- MatrixRotate/Program.cs
+++ MatrixRotate/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace MatrixRotate
+namespace MatrixOps
 {
     class Program
     {
@@ -9,23 +9,25 @@ namespace MatrixRotate
         {
             Console.WriteLine("Matrix Related Methods");
 
+            var matrix = new MatrixOps();
+
             int size = 3;
             var dataMatrix = new List<List<int>>();
             var identityMatrix = new List<List<int>>();
             var resultMatrix = new List<List<int>>();
 
-            // Create a matrix of size size and rotate 90 degrees. 
+            // Create a matrix of size and rotate 90 degrees. 
             CreateDataMatrix(dataMatrix, size, 10);
-            ListMatrix(dataMatrix, size, "Data Matrix");
-            MatrixRotate_90(dataMatrix, size);
-            ListMatrix(dataMatrix, size, "Data matrix rotate 90 degree clockwise");
+            matrix.List(dataMatrix, size, "Data Matrix");
+            matrix.Rotate_90(dataMatrix, size);
+            matrix.List(dataMatrix, size, "Data matrix rotate 90 degree clockwise");
 
-            // Apply a matrix multiply operation with an identity matrix.
-            ListMatrix(dataMatrix, size, "Using Data Matrix");
-            CreateIdentityMatrix(identityMatrix, size);
-            ListMatrix(identityMatrix, size, "Multiply by Identity Matrix");
-            MatrixMultiply(resultMatrix, dataMatrix, size);
-            ListMatrix(resultMatrix, size, "Resulting Data Matrix");
+            // Multiply two maticies.
+            matrix.List(dataMatrix, size, "Multiply data matrix by identity Matrix");
+            identityMatrix = matrix.MakeLeftOnes(size);
+            matrix.List(identityMatrix, size, "");
+            resultMatrix = matrix.Multiply(dataMatrix, matrix.MakeLeftOnes(size), size);
+            matrix.List(resultMatrix, size, "for result ");
 
             Console.ReadKey();
         }
@@ -46,77 +48,13 @@ namespace MatrixRotate
             }
         }
 
-       private static int MatrixRotate_90(List<List<int>> matrix, int size)
-        {
-            int tail;
 
-            for (int box =0; box<size/2; box++)
-                for (int runner=box; runner < size-box-1; runner++)
-                {
-                    tail = matrix[box][runner];
-                    matrix[box][runner]               = matrix[size-runner-1][box];        //TL
-                    matrix[size-runner-1][box]        = matrix[size-box-1][size-runner-1]; //BL
-                    matrix[size-box-1][size-runner-1] = matrix[runner][size-box-1];        //BR
-                    matrix[runner][size-box-1]        = tail;                              //TR
-                }
 
-            return 0;
-        }
 
-        private static void ListMatrix(List<List<int>> matrix, int size, string msg)
-        {
-            Console.WriteLine();
-            Console.WriteLine(msg);
+       
 
-            for (int i = 0; i < size; i++)
-            { 
-                for (int j = 0; j < size; j++)
-                    Console.Write(matrix[i][j].ToString() + " ");
-                Console.WriteLine();
-            }
-        }
 
-        private static void CreateIdentityMatrix(List<List<int>> matrix, int size)
-        {
-            int cnt;
-            for (int i = 0; i < size; i++)
-            {
-                var vi = new List<int>();
-                cnt = 0;
 
-                for (int j=0; j<size; j++)
-                {
-                   if (cnt < i+1) {
-                        vi.Insert(j, 1);
-                        cnt++;
-                   }
-                   else      
-                        vi.Insert(j, 0);
-                }
 
-                matrix.Insert(i, vi);
-            }
-        }
-
-        private static void MatrixMultiply(List<List<int>> resultMatrix, List<List<int>> srcMatrix, int size)
-        {
-            var identityMatrix = new List<List<int>>();
-            CreateIdentityMatrix(identityMatrix, size);
-
-            for (int i = 0; i < size; i++)
-            {
-                var row = new List<int>();
-                for (int j = 0; j < size; j++)
-                {
-                    int tmp = 0;
-
-                    for (int r = 0; r < size; r++)
-                        tmp += srcMatrix[i][r] * identityMatrix[r][j];
-
-                    row.Insert(j, tmp);
-                }
-                resultMatrix.Insert(i, row);
-            }
-        }
     }
 }
